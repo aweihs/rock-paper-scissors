91cd3f8f7f773c5073761114386d7d3d5b552c7b
Ant Weihs <54947317+aweihs@users.noreply.github.com>
2024-02-15 15:23:42 -0500

    Initialize user choice function
    Function for user choice initialized, using loops and user input to declare users choice for the game.

--- javascript.js
+++ javascript.js
@@ -12,4 +12,32 @@ function getComputerChoice() {
 
 }
 
+//uses user input to get real players choice for rock, paper, or scissors
+function getPlayerChoice() {
+
+    let playerOption = false;
+
+    while (playerOption === false) { //while loop runs until player selects proper input
+
+        let playerChoice = prompt('Please choose rock, paper, or scissors.')
+        let playerPiece = playerChoice.toLowerCase();
+
+        if (playerPiece === 'rock') {
+            playerOption = true;
+            return 'Rock';
+        } else if (playerPiece === 'paper') {
+            playerOption = true;
+            return 'Paper';
+        } else if (playerPiece === 'scissors') {
+            playerOption = true;
+            return 'scissors';
+        } else {
+            playerOption = false;
+            alert('Invalid input, please try again.')
+        }
+    }
+}
+
+console.log(getPlayerChoice())
+
 console.log(getComputerChoice())
