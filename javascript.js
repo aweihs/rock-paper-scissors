405dd0f8422a861f01459d787cf539521a6d8a0b
Ant Weihs <54947317+aweihs@users.noreply.github.com>
2024-02-17 11:45:44 -0500

    Initialize playGame function and test
    Function to playGame using a loop with multiple playRound calls created and initialized, as well as tested. Round counter and player score is displayed with a winner made at having 5 wins.

--- javascript.js
+++ javascript.js
@@ -35,31 +35,95 @@ function getPlayerChoice() {
             playerOption = false;
             alert('Invalid input, please try again.')
         }
+
     }
+
 }
 
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-
+//uses user input as parameters to play a single round of rock paper scissors
 function playRound(computerSelection, playerSelection) {
 
-    if (computerSelection === playerSelection) {
-        return 'You have tied! Try again.';
+    playerWin = 'win';
+    computerWin = 'loss';
+    tieGame = 'tie';
+
+    if (computerSelection === playerSelection) { //all options for round played
+        console.log('You have tied this round! Try again.');
+        return tieGame;
     } else if (computerSelection === 'rock' && playerSelection === 'paper') {
-        return 'You win! Paper beats rock. Well done!';
+        console.log('You win this round! Paper beats rock. Well done!');
+        return playerWin;
     } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        return 'You lose. Rock beats scissors. Better luck next time!';
+        console.log('You lose this round. Rock beats scissors. Better luck next time!');
+        return computerWin;
     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        return 'You lose. Paper beats rock. Better luck next time!';
+        console.log('You lose this round. Paper beats rock. Better luck next time!');
+        return computerWin;
     } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-        return 'You win! Scissors beat paper. Well done!';
+        console.log('You win this round! Scissors beat paper. Well done!');
+        return playerWin;
     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        return 'You lose! Scissors beat paper. Better luck next time!';
+        console.log('You lose this round! Scissors beat paper. Better luck next time!');
+        return computerWin;
     } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-        return 'You win! Rock beats scissors. Well done!';
+        console.log('You win this round! Rock beats scissors. Well done!');
+        return playerWin;
     }
 
 }
 
-console.log(playRound(computerSelection, playerSelection));
+//full game of rock paper scissors function using computerChoice, playerChoice, and playRound functions.
+//the game keeps score and is over when a player reaches 5 points.
+function playGame() {
 
+    let computerScore = 0;
+    let playerScore = 0;
+    let roundNumber = 1;
+    let gameOver = false;
+
+    while(gameOver === false) { //while loop tracks if game is still playing, won't end until win condition is met
+
+        if(playerScore < 5 && computerScore < 5){ //if statement for actual gameplay, won't move past this until a side wins
+
+            console.log('Round ' + roundNumber);
+
+            let computerSelection = getComputerChoice();
+            let playerSelection = getPlayerChoice();
+    
+            let roundWinner = playRound(computerSelection, playerSelection); //starts the round and determines who wins and loses
+
+            if(roundWinner === 'win') { //conditions for win and loss
+                playerScore++;
+            } else if(roundWinner === 'loss') {
+                computerScore++;
+            }
+
+            //BELOW when gameOver is set to true, while loop aka game ends
+
+        } else if(playerScore >= 5) { //if player wins
+
+            console.log('You win the game, congratulations!')
+            gameOver = true;
+
+        } else if(computerScore >= 5) { //if computer wins
+
+            console.log('You lose the game! Thanks for playing.')
+            gameOver = true;
+
+        } else { //Unachievable, but important for if ever it were to happen
+
+            console.log('Tie game, how could you possibly achieve this?')
+            gameOver = true;
+
+        }
+
+        console.log('Player 1 Score: ' + playerScore); //displays player score at the end of each round
+        console.log('Computer Score: ' + computerScore);
+
+        roundNumber++; //increments round number being displayed up by 1
+
+    }
+
+}
+
+playGame();
